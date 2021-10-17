456e863234f05447416aacfd3f5631bec15fae64
insane 3000 <you@example.com>
2021-10-17 19:32:30 -0400

    asds

--- dist/app.js
+++ dist/app.js
@@ -14,10 +14,10 @@ const book_routes_1 = __importDefault(require("./routes/book/book.routes"));
 const products_routes_1 = __importDefault(require("./routes/products/products.routes"));
 const cashRegister_routes_1 = __importDefault(require("./routes/cashRegister/cashRegister.routes"));
 const auth_routes_1 = __importDefault(require("./routes/auth/auth.routes"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 var clientURI = {
-    origin: [`${process.env.URI}`],
+    origin: ["http://181.115.166.214:3000", "http://localhost:3000"],
 };
 const app = (0, express_1.default)();
 app.set("port", config_1.default.PORT);
