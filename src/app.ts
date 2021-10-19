aac600fbd23cbebdb6fa24cc4ce7443eb215d371
insane 3000 <you@example.com>
2021-10-18 20:21:03 -0400

    asd

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,8 @@ var clientURI = {
   origin: [
     "http://192.168.0.148:3000",
     "http://localhost:3000",
-    "http://navigamescbba.com/#/",
-    "https://navigamescbba.com/#/",
+    "http://navigamescbba.com",
+    "https://navigamescbba.com",
   ],
 };
 const app = express();
