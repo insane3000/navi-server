6ff619dcc26acf98cd088601df03040cabaddbbb
insane 3000 <you@example.com>
2021-10-19 17:03:40 -0400

    asdasd

--- dist/app.js
+++ dist/app.js
@@ -20,6 +20,7 @@ var clientURI = {
     origin: [
         // "http://192.168.0.148:3000",
         "http://0.0.0.0:3000",
+        "http://localhost:3000",
         // "https://navigamescbba.com",
     ],
 };
