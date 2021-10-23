f6e1790694369a219968a0b8994af769c45c5bc8
insane 3000 <you@example.com>
2021-10-23 15:30:44 -0400

    finalizado

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -6,13 +6,46 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 // !Datos del perfil
 export const profile: RequestHandler = async (req, res) => {
-  const user = await User.findById(req.body._id);
+  console.log(req.params);
+
+  const user = await User.findById(req.params.id);
   if (!user) {
     return res.status(400).json("No user found");
   }
   res.json(user);
 };
+// !Actualizar perfil
+export const updateProfile: RequestHandler = async (req, res) => {
+  const user = await User.findById(req.body._id);
+  if (!user) {
+    return res.status(400).json("usuario no encontrado o datos incorrectos");
+  }
+  const correctPassword: boolean = await user.validatePassword(
+    req.body.oldPassword
+  );
+  if (!correctPassword) {
+    return res.status(400).json("invalid password");
+  }
+  const newPassword: UserIT = new User({
+    password: req.body.newPassword,
+  });
 
+  newPassword.password = await newPassword.encryptPassword(
+    newPassword.password
+  );
+
+  const userUpdated = await User.findByIdAndUpdate(
+    req.body._id,
+    { password: newPassword.password },
+    {
+      new: true,
+    }
+  );
+  console.log(newPassword);
+  if (!userUpdated) return res.status(204).json();
+  return res.json(userUpdated);
+  // return res.json("lalalalalalala");
+};
 // !para registar usuario
 export const signup: RequestHandler = async (req, res) => {
   const newUser: UserIT = new User({
--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -3,8 +3,9 @@ import * as authCtrl from "./auth.controller";
 import { tokenValidation } from "../../libs/validateToken";
 
 const router = Router();
-// router.post("/signup", authCtrl.signup);
+router.post("/signup", authCtrl.signup);
 router.post("/login", authCtrl.login);
-// router.get("/profile", tokenValidation, authCtrl.profile);
+router.get("/profile/:id", authCtrl.profile);
+router.put("/profile/:id", authCtrl.updateProfile);
 
 export default router;
